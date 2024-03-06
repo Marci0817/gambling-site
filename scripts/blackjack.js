457746054f489717050f0a6593e67ef21f71f4c7
Marci <mihmar17@gmail.com>
2024-03-06 17:32:02 +0100

    WIP: add player cards, add actions, add dealer dealing

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -1,12 +1,111 @@
-/*let allCardNumbers = 0; //all card number
-
-function addCardNumber(card) {
-    allCardNumbers += card;
-    let cardNumberValue = document.getElementById("cardNumber");
-    cardNumberValue.innerText = allCardNumbers;
-}*/
+const DELAY_BETWEEN_CARDS = 500;
 
 let playerBet = 0; //Player current bet
+
+let dealerCards = [];
+let playerCards = [];
+
+async function startNewGame() {
+    let startButton = document.getElementById("startButton");
+    startButton.classList.add("hidden");
+
+    //Make request to server to start new game and get the cards
+    /* ...later.. */
+    addPlayerCard([generateRandomCard(), generateRandomCard()]);
+    await sleep(2 * DELAY_BETWEEN_CARDS); // 2* DELAY_BETWEEN_CARDS, 2 because we have 2 cards
+    revealDealerCard();
+
+    //User interaction needed
+    let playerInteraction = document.getElementById("playerInteractions");
+    playerInteraction.classList.remove("hidden");
+}
+
+function playerAction(action) {
+    let playerInteraction = document.getElementById("playerInteractions");
+    playerInteraction.classList.add("hidden");
+    switch (action) {
+        case "hit":
+            //later request
+            break;
+        case "stand":
+            //later request
+            break;
+        case "double":
+            //later request
+            break;
+        case "split":
+            //later request
+            break;
+        default:
+            break;
+    }
+
+    //for now generate random card
+    addPlayerCard([generateRandomCard()]);
+}
+
+async function addPlayerCard(cards) {
+    let playerCardBoard = document.getElementById("playerCardBoard");
+
+    let defaultCards = playerCardBoard.children;
+
+    for (let i = 0; i < cards.length; i++) {
+        let isFirstCards = false;
+        //First two cards reveal
+        if (
+            defaultCards[i].attributes.src.nodeValue ===
+            "assets/cards/cardback.webp"
+        ) {
+            defaultCards[i].attributes.src.nodeValue = cards[i];
+            isFirstCards = true;
+        }
+
+        if (!isFirstCards) {
+            let newPlayerCard = document.getElementById("playerCardBoard");
+            let newCard = document.createElement("img");
+            newCard.src = cards[i];
+            newPlayerCard.appendChild(newCard);
+        }
+        newCardSound();
+        await sleep(DELAY_BETWEEN_CARDS);
+    }
+}
+
+function revealDealerCard() {
+    let dealerCardBoard = document.getElementById("dealerCardBoard");
+    let defaultDealerCards = dealerCardBoard.children;
+
+    //Reveal the first dealer card
+    for (let i = 0; i < 1; i++) {
+        defaultDealerCards[i].attributes.src.nodeValue = generateRandomCard();
+        newCardSound();
+    }
+}
+
+// Helper functions
+
+function getSoundValue() {
+    let checkbox = document.getElementById("soundOnOff");
+    return checkbox.checked;
+}
+
+function setPlayerBetCount(value) {
+    let playerBetCount = document.getElementById("playerChips");
+    playerBetCount.innerText = value;
+}
+
+function newCardSound() {
+    if (getSoundValue()) {
+        let audio = new Audio("/assets/sounds/newcardintable.mp3");
+        audio.play();
+    }
+}
+
+async function sleep(interval) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, interval);
+    });
+}
 
 //Add credit to player
 function addCredit(credit) {
@@ -38,46 +137,6 @@ function addCredit(credit) {
         }
     }
     setPlayerBetCount(playerBet);
-}
-
-let dealerCards = [];
-let playerCards = [];
-
-async function startNewGame() {
-    //Make request to server to start new game and get the cards
-    /* ...later.. */
-
-    let playerCardBoard = document.getElementById("playerCardBoard");
-
-    let defaultCards = playerCardBoard.children;
-
-    //Reveal first two cards
-    for (let i = 0; i < defaultCards.length; i++) {
-        setTimeout(() => {
-            defaultCards[i].attributes.src.nodeValue = generateRandomCard();
-            newCardSound();
-        }, i * 500);
-    }
-    console.log(defaultCards);
-}
-
-// Helper functions
-
-function getSoundValue() {
-    let checkbox = document.getElementById("soundOnOff");
-    return checkbox.checked;
-}
-
-function setPlayerBetCount(value) {
-    let playerBetCount = document.getElementById("playerChips");
-    playerBetCount.innerText = value;
-}
-
-function newCardSound() {
-    if (getSoundValue()) {
-        let audio = new Audio("/assets/sounds/newcardintable.mp3");
-        audio.play();
-    }
 }
 
 //For testing generate random card
