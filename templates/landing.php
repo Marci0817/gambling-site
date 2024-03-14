f16729ad40e1e5b9b6188425737d289745473d47
Nyéki Máté Gyula <sodium584@gmail.com>
2024-03-14 11:17:34 +0100

    Break up landing into templates

---
+++ templates/landing.php
@@ -0,0 +1,43 @@
+<?php
+$title = "fasz";
+
+$styles = [
+    "landing"
+];
+
+$scripts = [
+    "cards" => [],
+    "navbar" => ["defer"]
+];
+?>
+
+<?php ob_start(); ?>
+<?php include("navbar.php"); ?>
+<div id="tagline">
+    <h1 class="font-display">
+        The most <b>sophisticated</b><br />
+        gambling experience.
+    </h1>
+    <div id="cards-overlay">
+    </div>
+</div>
+<div class="section">
+    <h2 class="font-display section-title">Blackjack</h2>
+    <p>
+        Try yourself in the most popular card game (after poker) of all
+        time. The rules are simple, but the game is complex. Beat the
+        dealer and win big!
+    </p>
+    <a href="/auth.php" class="btn section-cta">Get started.</a>
+</div>
+<script>
+    const cardsOverlay = document.getElementById('cards-overlay');
+
+    cardsOverlay.appendChild(createCard(14, 's'));
+    cardsOverlay.appendChild(createCard(8, 'c'));
+    cardsOverlay.appendChild(createCard(3, 's'));
+</script>
+<?php
+$slot = ob_get_clean();
+include("templates/main.php");
+?>
---
+++ templates/main.php
@@ -0,0 +1,24 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="gambling-site" />
+    <meta name="keywords" content="gambling, site" />
+    <meta name="author" content="Mihály Marcell, Nyéki Máté Gyula" />
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="/styles/global.css">
+    <?php foreach ($styles as $style) : ?>
+        <link rel="stylesheet" href="/styles/<?= $style ?>.css">
+    <?php endforeach; ?>
+
+    <?php foreach ($scripts as $script => $opts) : ?>
+        <script src="/scripts/<?= $script ?>.js" <?= implode(" ", $opts); ?>></script>
+    <?php endforeach; ?>
+</head>
+
+<body>
+    <?= $slot ?>
+</body>
+
+</html>
