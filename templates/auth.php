c7b099abbcc50146092139c43f8e7d769ef67cb4
Marci0817 <mihmar17@gmail.com>
2024-03-18 18:00:41 +0100

    Refactor structure for all

---
+++ templates/auth.php
@@ -0,0 +1,48 @@
+<?php
+$title = "Login register page";
+
+$styles = [
+    "global",
+    "auth"
+];
+
+$scripts = [
+    "auth" => ["defer"]
+];
+?>
+
+<?php ob_start(); ?>
+<div class="">
+    <form id="loginForm" action="/auth.php" method="post">
+        <h1>Login</h1>
+        <input type="text" name="username" placeholder="Username" required autocomplete="username" />
+        <input type="password" name="password" placeholder="Password" required autocomplete="current-password" />
+        <button class="btn" type="submit" name="login">Login</button>
+        <p class="note">
+            Are you not registered yet?
+            <button type="button" class="reset-btn" onclick="selectForm('register')">
+                Register
+            </button>
+        </p>
+    </form>
+
+    <form id="registerForm" action="/auth.php" method="post" class="hidden">
+        <h1>Register</h1>
+        <input type="text" name="username" placeholder="Username" required autocomplete="username" />
+        <input type="password" name="password" placeholder="Password" required autocomplete="new-password" />
+        <input type="password" name="password2" placeholder="Password again" required autocomplete="new-password" />
+        <button class="btn" type="submit" name="register">
+            Register
+        </button>
+        <p class="note">
+            Do you have an account?
+            <button type="button" class="reset-btn" onclick="selectForm('login')">
+                Login
+            </button>
+        </p>
+    </form>
+</div>
+<?php
+$slot = ob_get_clean();
+include("templates/main.php");
+?>
---
+++ templates/coinflip.php
@@ -0,0 +1,35 @@
+<?php
+$title = "Coinflip";
+
+$styles = [
+    "global",
+    "coinflip"
+];
+
+$scripts = [
+    "coinflip" => ["defer"]
+];
+?>
+
+<?php ob_start(); ?>
+
+<div id="coinFlip">
+    <div id="imageContainer">
+        <img src="/assets/coins/head.png" id="flipHead" />
+    </div>
+    <div id="hudContainer">
+        <h1>CoinFlip</h1>
+        <p>Choose heads or tails and flip the coin!</p>
+        <input type="number" min="1" id="bet" placeholder="Bet amount" />
+        <h3>Choose a side</h3>
+        <div id="sides">
+            <img src="/assets/coins/head.png" id="head" class="icon" onclick="selectSide('head')" />
+            <img src="/assets/coins/tail.png" id="tail" class="icon" onclick="selectSide('tail')" />
+        </div>
+        <button onclick="flipCoin()">Flip the coin</button>
+    </div>
+</div>
+<?php
+$slot = ob_get_clean();
+include("templates/main.php");
+?>
