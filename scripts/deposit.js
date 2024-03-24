87a3de2cfb6afc58b33f97d08cfb4325e9e24cca
Nyéki Máté Gyula <sodium584@gmail.com>
2024-03-24 15:09:16 +0100

    add numberToSzamnev

--- scripts/deposit.js
+++ scripts/deposit.js
@@ -23,13 +23,24 @@ function tick(delta) {
         el.innerText = "$" + value;
         el.classList.add("bubble");
         container.appendChild(el);
-        
+        el.onclick = () => {
+            el.innerHTML = numberToSzamnev(value);
+        };
+
+        const x = Math.floor(Math.random() * container.clientWidth);
+        const y = Math.floor(Math.random() * container.clientHeight);
+
+        const angle = Math.atan2(
+            container.clientWidth / 2 - x,
+            container.clientHeight / 2 - y
+        );
+
         bubbles.push({
-            value: value,
-            angle: Math.random() * Math.PI * 2,
-            x: Math.floor(Math.random() * container.clientWidth),
-            y: Math.floor(Math.random() * container.clientHeight),
-            el: el,
+            value,
+            angle,
+            x,
+            y,
+            el,
         });
     }
 
@@ -38,9 +49,113 @@ function tick(delta) {
         bubble.y += Math.sin(bubble.angle) * delta * 0.03;
         bubble.el.style.transform = `translate(${bubble.x}px, ${bubble.y}px)`;
 
-        if (bubble.x < -96 || bubble.y < -96 || bubble.x > container.clientWidth || bubble.y > container.clientHeight) {
+        if (
+            bubble.x < -96 ||
+            bubble.y < -96 ||
+            bubble.x > container.clientWidth ||
+            bubble.y > container.clientHeight
+        ) {
             bubble.el.remove();
             bubbles = bubbles.filter((b) => b !== bubble);
         }
     }
 }
+
+let prefixes = [
+    "",
+    "két",
+    "három",
+    "négy",
+    "öt",
+    "hat",
+    "hét",
+    "nyolc",
+    "kilenc",
+];
+let suffixes = [
+    "egy",
+    "kettő",
+    "három",
+    "négy",
+    "öt",
+    "hat",
+    "hét",
+    "nyolc",
+    "kilenc",
+];
+
+let thousand = "ezer";
+let hundred = "száz";
+let tensPrefixes = [
+    "tizen",
+    "huszon",
+    "harminc",
+    "negyven",
+    "ötven",
+    "hatvan",
+    "hetven",
+    "nyolcvan",
+    "kilencven",
+];
+
+let tens = [
+    "tíz",
+    "húsz",
+    "harminc",
+    "negyven",
+    "ötven",
+    "hatvan",
+    "hetven",
+    "nyolcvan",
+    "kilencven",
+];
+
+function numberToSzamnev(number) {
+    const str = number.toString();
+    let res = "";
+
+    let prevNonZero = 0;
+    for (let i = 0; i < str.length; i++) {
+        const digit = parseInt(str.charAt(i));
+        let nextDigit = 0;
+        if (i < str.length - 1) {
+            nextDigit = parseInt(str.charAt(i + 1));
+        }
+
+        if (digit > 0 && prevNonZero >= 2000) {
+            res += "-";
+        }
+
+        switch (str.length - i) {
+            case 4:
+                res += prefixes[digit - 1] + thousand;
+                break;
+            case 3:
+                if (digit > 0) {
+                    res += prefixes[digit - 1] + hundred;
+                }
+                break;
+            case 2:
+                if (digit > 0) {
+                    if (nextDigit > 0) {
+                        res += tensPrefixes[digit - 1];
+                    } else {
+                        res += tens[digit - 1];
+                    }
+                }
+                break;
+            case 1:
+                if (digit > 0) {
+                    res += suffixes[digit - 1];
+                }
+        }
+
+        if (digit != 0) {
+            prevNonZero = digit * 10 ** (str.length - i - 1);
+        }
+    }
+
+    return res;
+}
+
+for (let i = 0; i <= 3000; i++) console.log(numberToSzamnev(i));
