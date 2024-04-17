8d5393f7e01b632c21fa0e46d6775a461c5dc051
Nyéki Máté Gyula <sodium584@gmail.com>
2024-04-17 17:36:36 +0200

    Add BlackjackGame

---
+++ utils/BlackjackGame.php
@@ -0,0 +1,111 @@
+<?php
+
+class BlackjackGame
+{
+    private bool $isOver = false;
+
+    private string $username;
+
+    private string $bet;
+
+    private array $deck = [];
+    private array $dealerHand = [];
+    private array $playerHand = [];
+
+    public function __construct(string $username, string $bet)
+    {
+        $this->username = $username;
+        $this->bet = $bet;
+
+        $suits = ["c", "d", "h", "s"];
+
+        foreach ($suits as $suit) {
+            for ($i = 2; $i <= 14; $i++) {
+                $this->deck[] = $i . $suit;
+            }
+        }
+
+        shuffle($this->deck);
+
+        $this->addPlayerCard();
+        $this->addDealerCard();
+    }
+
+
+    public function hit(): void
+    {
+        if ($this->isOver || $this->getPlayerHandValue() >= 21) {
+            return;
+        }
+
+        $this->addPlayerCard();
+    }
+
+    public function stand(): void
+    {
+        if ($this->isOver) {
+            return;
+        }
+
+        while ($this->getDealerHandValue() < 17) {
+            $this->addDealerCard();
+        }
+
+        $this->isOver = true;
+    }
+
+    public function isOver(): bool
+    {
+        return $this->isOver;
+    }
+
+    public function getPlayerHand(): array
+    {
+        return $this->playerHand;
+    }
+
+    public function getPlayerHandValue(): int
+    {
+        return $this->getHandValue($this->playerHand);
+    }
+
+    public function getDealerHandValue(): int
+    {
+        return $this->getHandValue($this->dealerHand);
+    }
+
+    public function getDealerHand(): array
+    {
+        return $this->dealerHand;
+    }
+
+    private function addPlayerCard(): void
+    {
+        $card = array_pop($this->deck);
+        $this->playerHand[] = $card;
+    }
+
+    private function addDealerCard(): void
+    {
+        $card = array_pop($this->deck);
+        $this->dealerHand[] = $card;
+    }
+
+    private function getHandValue(array $cards): int
+    {
+        $sum = 0;
+
+        foreach ($cards as $card) {
+            $val = intval(substr($card, 0, -1));
+            if ($val == 14) {
+                $sum += 11;
+            } else if ($val > 10) {
+                $sum += 10;
+            } else {
+                $sum += $val;
+            }
+        }
+
+        return $sum;
+    }
+}
